7105e56259f7e43b2d11de98aa6ef80bcfc0a5a1
hway0ung <real_khy@naver.com>
2022-12-07 21:33:47 +0900

    unit1 - TaskCompleted

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -22,15 +22,68 @@ import com.example.taskcompleted.ui.theme.TaskCompletedTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            TaskCompletedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TaskManageApp()
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun TaskCompletedScreen() {
-    Column( ) { }
+fun TaskManageApp() {
+    TaskManage(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        mainText = "All tasks completed",
+        subText = "Nice work!"
+    )
+}
+
+@Composable
+fun TaskManage(image: Painter, mainText: String, subText: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = mainText,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = subText,
+            fontSize = 16.sp,
+        )
+
+
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    TaskCompletedTheme {
+        TaskManageApp()
+    }
+}
